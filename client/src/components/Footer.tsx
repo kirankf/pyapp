9102bc4c003b4be5951f9682e75a698da8272685
polymorphisma <shrawansunar.6@gmail.com>
2024-07-16 17:07:49 +0545

    update:: changed footer values

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -3,21 +3,21 @@ import GmailIcon from '../assets/gmail.svg';
 import LinkedInIcon from '../assets/linkedin.svg';
 
 const Footer = () => {
-  const gmailLink = 'mailto:mdmahfuzurrahmanarif@gmail.com';
-  const githubLink = 'https://github.com/mahfuzurrahman98';
+  const gmailLink = 'mailto:info@adex.com';
+  const githubLink = 'https://github.com/adexltd';
   const linkedinLink =
-    'https://www.linkedin.com/in/md-mahfuzur-rahman-arif-5643b6146';
-
+    'https://www.linkedin.com/company/adexltd';
+  const websiteLink = "https://adex.ltd/"
   return (
     <div className="flex flex-col justify-center mt-auto">
       <div className="border-t-2 mb-2 border-gray-200 py-3 mt-12">
         <p className="text-center">
           <a
-            href="https://mahfuzurrahman98.github.io"
+            href={websiteLink}
             target="blank"
             className="text-green-600 font-semibold"
           >
-            Mahfuzur Rahman Arif
+            Adex International
           </a>
           <span className="ml-2">
             | All rights reserved, {new Date().getFullYear()}
